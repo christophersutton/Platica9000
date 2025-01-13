83dcf9a02362145898e3de619238f9e52e68629b
Chris Sutton <chris_sutton@hey.com>
2025-01-13 08:49:06 -0600

    removes providers for real

--- packages/client/src/components/Auth.jsx
+++ packages/client/src/components/Auth.jsx
@@ -12,6 +12,7 @@ export default function AuthComponent() {
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
           theme="dark"
+          providers={[]}
         />
       </div>
     </div>
